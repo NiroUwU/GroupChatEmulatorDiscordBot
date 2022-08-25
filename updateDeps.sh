809ed9f3547989b9c89f4b4d6c6790ccce7660ef
NiroUwU <rostiplotni@gmail.com>
2022-08-25 22:48:07 +0200

    added deps updater

---
+++ updateDeps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script updates all the dependancies
+
+LIT_DEPS=( 'SinisterRectus/discordia' )
+
+CMDS=()
+
+
+# Update all lit dependancies:
+for package in "${LIT_DEPS[@]}"; do
+	lit install "$package"
+done
+
+# Execute other update commands:
+for command in "${CMDS[@]}"; do
+	$command
+done
